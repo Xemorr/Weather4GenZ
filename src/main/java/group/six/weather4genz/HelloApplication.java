6d821cb976f9a6a63f5576e24b2c025edeac12cd
samue <xemoryt@gmail.com>
2022-05-10 14:55:20 +0100

    Renamed application window

--- src/main/java/group/six/weather4genz/HelloApplication.java
+++ src/main/java/group/six/weather4genz/HelloApplication.java
@@ -9,17 +9,20 @@ import java.io.IOException;
 
 public class HelloApplication extends Application {
 
+    public static WeatherDataHandler weatherDataHandler; //object that handles API calls
+
     @Override
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
         Scene scene = new Scene(fxmlLoader.load(), 607, 800);
-        stage.setTitle("Hello!");
+        stage.setTitle("Weather4GenZ");
         stage.setScene(scene);
         stage.show();
     }
 
     public static void main(String[] args) {
         String token = args[0]; //The token for the openweatherapi
+        weatherDataHandler = new WeatherDataHandler(token);
         launch();
     }
 }
