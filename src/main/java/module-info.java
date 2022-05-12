006ed6e02d827212913bb137a9d6c2914c9e1934
samue <xemoryt@gmail.com>
2022-05-12 14:32:51 +0100

    Added ability to do API calls for locations and for the weather at a given location

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,8 +1,14 @@
 module group.six.weather4genz {
     requires javafx.controls;
     requires javafx.fxml;
+    requires okio;
+    requires okhttp3;
+    requires kotlin.stdlib;
+    requires kotlin.stdlib.common;
 
     requires org.kordamp.bootstrapfx.core;
+    requires annotations;
+    requires json.path;
 
     opens group.six.weather4genz to javafx.fxml;
     exports group.six.weather4genz;
